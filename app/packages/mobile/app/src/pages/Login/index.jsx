1c6e48bf9137f6b428977ea494fa4da23cd4171e
Vinny <vinicius.moraes@ativy.com>
2022-01-08 20:53:32 -0300

    chore: Reverse changes to deploy

--- app/packages/mobile/app/src/pages/Login/index.jsx
+++ app/packages/mobile/app/src/pages/Login/index.jsx
@@ -3,13 +3,12 @@ import React from "react"
 import { Route } from "react-router-dom"
 import LoginComponent from "../../components/Login"
 import SignUpComponent from "../../components/Login/SignUp"
-import SearchComponent from "../../components/Search"
 
 const LoginPage = () => {
   return (
     <>
       <Route exact path="/sign-up" component={SignUpComponent} />
-      <Route exact path="/" component={SearchComponent} />
+      <Route exact path="/" component={LoginComponent} />
     </>
   )
 }
